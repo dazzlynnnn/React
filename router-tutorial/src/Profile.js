d35f2b4d1e3dec441cbcc0f5c46429a099b7b372
Haerin Lee <dazzlynnnn@gmail.com>
2021-10-06 21:31:42 +0900

    13장 - 리액트 라우터로 SPA 개발하기

---
+++ router-tutorial/src/Profile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const data = {
+    dazzlynnnn: {
+        name: '린',
+        description: '리액트 공부 중'
+    },
+    gildong: {
+        name: '홍길동',
+        description: '고전 소설의 주인공'
+    }
+};
+
+const Profile = ({ match }) => {
+    const { username } = match.params;
+    const profile = data[username];
+    if (!profile) { 
+        return <div>존재하지 않는 사용자입니다.</div>;
+    }
+    return (
+        <div>
+            <h3>
+                {username}({profile.name})
+            </h3>
+            <p>{profile.description}</p>
+        </div>
+    );
+};
+
+export default Profile;
